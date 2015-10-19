59c30d810181caf866d24ccf6ced80dc5e4d2aa8
Takanori Suzuki <takanorig@gmail.com>
2015-10-20 02:34:27 +0900

    camel-pahoの更新に合わせて、修正。

--- src/main/java/example/camel/paho/route/MqttRoute.java
+++ src/main/java/example/camel/paho/route/MqttRoute.java
@@ -1,9 +1,10 @@
 package example.camel.paho.route;
 
 import org.apache.camel.Exchange;
+import org.apache.camel.Message;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.paho.MqttProperties;
+import org.apache.camel.component.paho.PahoConstants;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,26 +12,23 @@ public class MqttRoute extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("paho:/test/topic?brokerUrl=tcp://test.mosquitto.org:1883").process(print);
+        from("paho:/test/+/sample/#?brokerUrl=tcp://test.mosquitto.org:1883").process(print);
     }
 
     private Processor print = new Processor() {
         @Override
         public void process(Exchange exchange) throws Exception {
-            Object header = exchange.getIn().getHeader("MqttProperties");
-            MqttProperties prop = null;
-            if (header instanceof MqttProperties) {
-                prop = (MqttProperties) header;
-            }
+            Object header = exchange.getIn().getHeader(PahoConstants.MQTT_TOPIC);
+            String topic = (String) header;
+            Message camelMessage = exchange.getIn();
 
-            byte[] body = (byte[]) exchange.getIn().getBody();
+            byte[] body = (byte[]) camelMessage.getBody();
             String payload = new String(body, "utf-8");
 
-            if (prop != null) {
-                System.out.println("topic=" + prop.getTopic() + ", payload=" + payload);
-            } else {
-                System.out.println("payload=" + payload);
-            }
+            System.out.println("topic=" + topic + ", payload=" + payload);
+
+            // can cast to PahoMessage
+            // PahoMessage pahoMessage = (PahoMessage) camelMessage;
         }
     };
 }
